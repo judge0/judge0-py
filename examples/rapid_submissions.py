f35c4241420da02b67357559e0e05155de0db25a
Filip Karlo Došilović <filipk.dosilovic@gmail.com>
2024-10-05 18:28:46 +0200

    Add functions for calls to create submissions batch and get submissions batch.

---
+++ examples/rapid_submissions.py
@@ -0,0 +1,42 @@
+import os
+import time
+
+import judge0
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("RAPID_API_KEY")
+
+
+def run_example(client_class, lang_id_python, lang_id_c):
+    client = client_class(api_key=api_key)
+    submission1 = judge0.SingleFileSubmission(
+        source_code="print('Hello Judge0')",
+        language_id=lang_id_python,
+        expected_output="Hello Judge0",
+    )
+    submission2 = judge0.SingleFileSubmission(
+        source_code='#include <stdio.h>\n\nint main() {\n  printf("Hello World!");\n  return 0;\n}',
+        language_id=lang_id_c,
+        expected_output="Hello World!",
+    )
+
+    submissions = [submission1, submission2]
+    client.create_submissions(submissions)
+    time.sleep(1)
+    client.get_submissions(submissions)
+
+    for submission in submissions:
+        print(f"{submission.status=}")
+        print(f"{submission.stdout=}")
+
+
+def main():
+    run_example(judge0.RapidJudge0CE, 100, 50)
+    run_example(judge0.RapidJudge0ExtraCE, 25, 1)
+
+
+if __name__ == "__main__":
+    main()
--- src/judge0/clients.py
+++ src/judge0/clients.py
@@ -15,11 +15,6 @@ class Client:
         except Exception:
             raise RuntimeError("Client authentication failed.")
 
-        # .....
-        # judge0.BASH = language_id
-        # for lang in BaseLanguage.values:
-        #     setattr(self.Language, lang, self.resolve_language(self.languages, lang))
-
     def get_about(self) -> dict:
         r = requests.get(
             f"{self.endpoint}/about",
@@ -56,7 +51,7 @@ class Client:
         r.raise_for_status()
         return r.json()
 
-    def create_submission(self, submission: Submission):
+    def create_submission(self, submission: Submission) -> None:
         # TODO: check if client supports specified language_id
         params = {
             "base64_encoded": "true",
@@ -67,15 +62,15 @@ class Client:
 
         resp = requests.post(
             f"{self.endpoint}/submissions",
-            headers=self.auth_headers,
-            params=params,
             json=body,
+            params=params,
+            headers=self.auth_headers,
         )
         resp.raise_for_status()
 
         submission.set_attributes(resp.json())
 
-    def get_submission(self, submission: Submission, *, fields=None):
+    def get_submission(self, submission: Submission, *, fields=None) -> None:
         """Check the submission status."""
 
         params = {
@@ -87,12 +82,52 @@ class Client:
 
         resp = requests.get(
             f"{self.endpoint}/submissions/{submission.token}",
-            headers=self.auth_headers,
             params=params,
+            headers=self.auth_headers,
         )
         resp.raise_for_status()
 
         submission.set_attributes(resp.json())
+
+    def create_submissions(self, submissions: list[Submission]) -> None:
+        params = {
+            "base64_encoded": "true",
+            "wait": str(self.wait).lower(),
+        }
+
+        submissions_body = [submission.to_dict() for submission in submissions]
+
+        resp = requests.post(
+            f"{self.endpoint}/submissions/batch",
+            headers=self.auth_headers,
+            params=params,
+            json={"submissions": submissions_body},
+        )
+        resp.raise_for_status()
+
+        for submission, attrs in zip(submissions, resp.json()):
+            submission.set_attributes(attrs)
+
+    def get_submissions(self, submissions: list[Submission], *, fields=None) -> None:
+        params = {
+            "base64_encoded": "true",
+        }
+
+        if fields is not None:
+            params["fields"] = ",".join(fields)
+
+        tokens = ",".join(submission.token for submission in submissions)
+        params["tokens"] = tokens
+
+        resp = requests.get(
+            f"{self.endpoint}/submissions/batch",
+            params=params,
+            headers=self.auth_headers,
+        )
+        resp.raise_for_status()
+
+        for submission, attrs in zip(submissions, resp.json()["submissions"]):
+            submission.set_attributes(attrs)
 
 
 class ATD(Client):
@@ -122,6 +157,8 @@ class ATDJudge0CE(ATD):
     DEFAULT_STATUSES_ENDPOINT: str = "c37b603f-6f99-4e31-a361-7154c734f19b"
     DEFAULT_CREATE_SUBMISSION_ENDPOINT: str = "6e65686d-40b0-4bf7-a12f-1f6d033c4473"
     DEFAULT_GET_SUBMISSION_ENDPOINT: str = "b7032b8b-86da-40b4-b9d3-b1f5e2b4ee1e"
+    DEFAULT_CREATE_SUBMISSIONS_ENDPOINT: str = "402b857c-1126-4450-bfd8-22e1f2cbff2f"
+    DEFAULT_GET_SUBMISSIONS_ENDPOINT: str = "e42f2a26-5b02-472a-80c9-61c4bdae32ec"
 
     def __init__(self, api_key):
         super().__init__(
@@ -150,13 +187,21 @@ class ATDJudge0CE(ATD):
         self._update_endpoint_header(self.DEFAULT_STATUSES_ENDPOINT)
         return super().get_statuses()
 
-    def create_submission(self, submission: Submission):
+    def create_submission(self, submission: Submission) -> None:
         self._update_endpoint_header(self.DEFAULT_CREATE_SUBMISSION_ENDPOINT)
         return super().create_submission(submission)
 
-    def get_submission(self, submission: Submission, *, fields=None):
+    def get_submission(self, submission: Submission, *, fields=None) -> None:
         self._update_endpoint_header(self.DEFAULT_GET_SUBMISSION_ENDPOINT)
         return super().get_submission(submission, fields=fields)
+
+    def create_submissions(self, submissions: Submission) -> None:
+        self._update_endpoint_header(self.DEFAULT_CREATE_SUBMISSIONS_ENDPOINT)
+        return super().create_submissions(submissions)
+
+    def get_submissions(self, submissions: Submission, *, fields=None) -> None:
+        self._update_endpoint_header(self.DEFAULT_GET_SUBMISSIONS_ENDPOINT)
+        return super().get_submissions(submissions, fields=fields)
 
 
 class ATDJudge0ExtraCE(ATD):
@@ -170,6 +215,8 @@ class ATDJudge0ExtraCE(ATD):
     DEFAULT_STATUSES_ENDPOINT: str = "a2843b3c-673d-4966-9a14-2e7d76dcd0cb"
     DEFAULT_CREATE_SUBMISSION_ENDPOINT: str = "be2d195e-dd58-4770-9f3c-d6c0fbc2b6e5"
     DEFAULT_GET_SUBMISSION_ENDPOINT: str = "c3a457cd-37a6-4106-97a8-9e60a223abbc"
+    DEFAULT_CREATE_SUBMISSIONS_ENDPOINT: str = "c64df5d3-edfd-4b08-8687-561af2f80d2f"
+    DEFAULT_GET_SUBMISSIONS_ENDPOINT: str = "5d173718-8e6a-4cf5-9d8c-db5e6386d037"
 
     def __init__(self, api_key):
         super().__init__(
@@ -205,6 +252,14 @@ class ATDJudge0ExtraCE(ATD):
     def get_submission(self, submission: Submission, *, fields=None):
         self._update_endpoint_header(self.DEFAULT_GET_SUBMISSION_ENDPOINT)
         return super().get_submission(submission, fields=fields)
+
+    def create_submissions(self, submission: Submission) -> None:
+        self._update_endpoint_header(self.DEFAULT_CREATE_SUBMISSIONS_ENDPOINT)
+        return super().create_submissions(submission)
+
+    def get_submissions(self, submission: Submission, *, fields=None) -> None:
+        self._update_endpoint_header(self.DEFAULT_GET_SUBMISSIONS_ENDPOINT)
+        return super().get_submissions(submission, fields=fields)
 
 
 class Rapid(Client):
